e2e3d303c1a977a4e51da0c5e2444839a1336b21
David Laganiere <40720561+davidlag0@users.noreply.github.com>
2023-06-12 08:26:48 -0400

    test(Bar): add tests to almost fully cover

--- frontend/__tests__/unit/components/Bar.test.jsx
+++ frontend/__tests__/unit/components/Bar.test.jsx
@@ -1,7 +1,8 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Bar from "components/Bar";
-import { Router } from "react-router-dom";
+import { MemoryRouter, Route, Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
+import userEvent from "@testing-library/user-event";
 
 // Useful reference: https://bholmes.dev/blog/mocking-browser-apis-fetch-localstorage-dates-the-easy-way-with-jest/
 
@@ -10,10 +11,10 @@ let mockStorage = {};
 describe("Bar", () => {
   beforeAll(() => {
     global.Storage.prototype.getItem = jest.fn((key) => mockStorage[key]);
+    global.Storage.prototype.clear = jest.fn(() => (mockStorage = {}));
   });
 
   beforeEach(() => {
-    // make sure the fridge starts out empty for each test
     mockStorage = {};
   });
 
@@ -43,5 +44,98 @@ describe("Bar", () => {
       </Router>
     );
     expect(container).toMatchSnapshot();
+  });
+
+  it("renders Bar unchanged when logged in, after clicking button to open menu", async () => {
+    const history = createMemoryHistory();
+    const user = userEvent.setup();
+    mockStorage["loggedIn"] = true;
+
+    const { container } = render(
+      <Router history={history}>
+        <Bar />
+      </Router>
+    );
+
+    const menuButton = screen.getByRole("button", {
+      name: "",
+    });
+
+    await user.click(menuButton);
+
+    const logOutMenuItem = screen.getByRole("menuitem", {
+      name: "Log out",
+    });
+    expect(logOutMenuItem).toBeInTheDocument();
+
+    expect(container).toMatchSnapshot();
+  });
+
+  test("open and close menu when logged in", async () => {
+    const history = createMemoryHistory();
+    const user = userEvent.setup();
+    mockStorage["loggedIn"] = true;
+
+    render(
+      <Router history={history}>
+        <Bar />
+      </Router>
+    );
+
+    const menuButton = screen.getByRole("button", {
+      name: "",
+    });
+
+    // Open menu
+    await user.click(menuButton);
+
+    const logOutMenuItem = screen.getByRole("menuitem", {
+      name: "Log out",
+    });
+    expect(logOutMenuItem).toBeInTheDocument();
+
+    // Close menu
+    await user.keyboard("{Escape}");
+
+    expect(logOutMenuItem).not.toBeInTheDocument();
+  });
+
+  test("open menu and click on 'Log out'", async () => {
+    const user = userEvent.setup();
+    mockStorage["loggedIn"] = true;
+    let testLocation;
+
+    render(
+      <MemoryRouter initialEntries={["/test-url/"]}>
+        <Route path="/test-url">
+          <Bar />
+        </Route>
+        <Route
+          path="*"
+          render={({ location }) => {
+            testLocation = location;
+            return null;
+          }}
+        />
+      </MemoryRouter>
+    );
+
+    const menuButton = screen.getByRole("button", {
+      name: "",
+    });
+
+    // Open menu
+    await user.click(menuButton);
+
+    const logOutMenuItem = screen.getByRole("menuitem", {
+      name: "Log out",
+    });
+    expect(logOutMenuItem).toBeInTheDocument();
+
+    // Click on 'Log out'
+    await user.click(logOutMenuItem);
+
+    expect(Object.keys(mockStorage).length).toBe(0);
+    expect(testLocation.pathname).toBe("/");
   });
 });
